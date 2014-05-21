7e7db02aed878b1d4357ee764270bf7658406662
André König <andre.koenig@gmail.com>
2014-05-21 15:35:15 +0200

    Initial Release.

---
+++ README.md
@@ -0,0 +1,25 @@
+# GitHub Markdown preview for [Zed](http://zedapp.org/)
+
+**Note:** This package is using a _bleeding edge_ API of Zed which is currently only available in the master branch.
+
+## Installation
+
+Install the package via [ZPM](http://zedapp.org/2014/05/zed-package-manager/).
+
+    gh:akoenig/zed-gfm-preview
+
+## Usage
+
+1. Open a Markdown file
+2. Open the preview split (Cmd+P or Ctrl+P)
+3. (Ignore the current rendered preview) Press Cmd+. or Ctrl+. and select GitHub:Markdown:Preview
+
+## Changelog
+
+### Version 1.0.0 (20140521)
+
+- Initial Release. Core functionality.
+
+## Author
+
+Copyright 2014, [André König](http://andrekoenig.info) (andre.koenig@posteo.de)
---
+++ index.js
@@ -0,0 +1,79 @@
+/*
+ * zed-gfm-preview
+ *
+ * Copyright(c) 2014 André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ */
+
+'use strict';
+
+var ui = require('zed/ui');
+var http = require('zed/http');
+var preview = require('zed/preview');
+var session = require('zed/session');
+
+/**
+ * GitHub Markdown Preview.
+ * 
+ */
+module.exports = function (info) {
+
+    var api = 'https://api.github.com/markdown/raw';
+    var stylesheet = 'https://raw.githubusercontent.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css';
+    
+    /**
+     * Creates the HTML template with the embedded styles.
+     * 
+     * @param {String} data The response body from the GitHub API.
+     * 
+     */
+    function template (data) {
+        var html = '<link rel="stylesheet" href="' + stylesheet + '">' +
+            '<style>'+
+                '.markdown-body {' +
+                    'min-width: 200px;' +
+                    'max-width: 790px;' +
+                    'margin: 0 auto;' +
+                    'padding: 30px;' +
+                '}' +
+            '</style>' +
+            '<article class="markdown-body">' +
+                 data +
+            '</div>';
+        
+        return html;
+    }
+    
+    
+    /**
+     * Passing the content from the current file to the
+     * GitHub Markdown API and present this in a preview split.
+     * 
+     * @param {String} content The content of the current file.
+     *
+     */
+    function onText (content) {
+        http.post(api, {
+            data: content,
+            headers: {'Content-Type': 'text/plain'}
+        }).then(
+            function onResponse (data) {
+                var html = template(data);
+        
+                preview.showPreview(html);
+            },
+            function onError (code) {
+                ui.prompt('Autsch! GitHub says: ' + code);
+            }
+        );
+    }
+
+    // Fetch the content from the current editor.
+    session.getText(info.path).then(onText);
+};
